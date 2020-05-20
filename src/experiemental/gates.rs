1d3e191c8b9c19bf425f458a619917da279e3fb9
Konstantce <kopanarin@yandex.ru>
2020-05-20 21:01:31 +0300

    WiP on optimized cs with state 4 and 8

--- src/experiemental/gates.rs
+++ src/experiemental/gates.rs
@@ -138,6 +138,7 @@ pub enum Index {
 /// used for statistics
 #[derive(Enum)]
 pub enum GateType {
+    // generation 1 gates
     EmptyGate,
     ConstantGate,
     AddGate,
@@ -148,6 +149,21 @@ pub enum GateType {
     LongLinearCombinationGate,
     SelectorGate,
     EqualityGate,
+
+    // generation 2 gates
+    DecomposeGate,
+    ComposeGate,
+    InvSelectorGate,
+    SubBytesGate,
+    MixClolumnGate,
+    RoundKeyAddUpdateGate,
+
+    // generation 3 gates
+    ComposeWideGate,
+    SubByteWideGate,
+    MixColumnsWideGate,
+    RoundKeyAddUpdateWideGate,
+    FinalHashUpdateWideGate,
 }
 
 
@@ -164,6 +180,20 @@ impl fmt::Display for GateType {
             GateType::LongLinearCombinationGate => "Long Linear Combination Gate",
             GateType::SelectorGate => "Selector Gate",
             GateType::EqualityGate => "Equality Gate",
+
+            GateType::DecomposeGate => "Decomposition Gate",
+            GateType::ComposeGate => "Composition Gate",
+            GateType::InvSelectorGate => "InvSelector Gate",
+            GateType::SubBytesGate => "SubBytes Gate",
+            GateType::MixClolumnGate => "MixColumnGate",
+            GateType::RoundKeyAddUpdateGate => "RoundKeyAddUpdateGate",
+
+            // generation 3 gates
+            GateType::ComposeWideGate => "ComposeWideGate",
+            GateType::SubByteWideGate => "SubByteWideGate",
+            GateType::MixColumnsWideGate => "MixColumnsWideGate",
+            GateType::RoundKeyAddUpdateWideGate => "RoundKeyAddUpdateWideGate",
+            GateType::FinalHashUpdateWideGate => "FinalHashUpdateGate",
         };
         
         write!(f, "{}", description)
@@ -207,6 +237,7 @@ pub enum Gate<Fr: BinaryField> {
     EqualityGate([Variable; 2]),
 
     // Used in more optimized version of constraint system (of width 4)
+    // used for 192-bit field with Davis-Meyer of Rijndael
     // let call it CS version 2
 
     //uses out from previous step
@@ -215,12 +246,12 @@ pub enum Gate<Fr: BinaryField> {
     // this gadget asserts that
     // P = c0 * P0 + c1 * P1 + c2 * P2 + c3 * P3
     // for some predefined constants [c0, c1, c3, c3]
-    DecomposeGadget([Variable; 5], [Coeff<Fr>; 4]),
+    DecomposeGate([Variable; 5]),
 
     // inverse to the previous operation: 
     // arguments are the same: [P, P0, P1, P2, P3] as well as the asserion check
     // however, this time we assume that P3 is defined on the previous step
-    ComposeGagdet([Variable; 5], [Coeff<Fr>; 4]),
+    ComposeGate([Variable; 5]),
 
     // InvSelect gadget: returns x^{-1} if x is invertible or zero instead
     // for width four the state is (x, x_inv, flag, out)
@@ -228,7 +259,7 @@ pub enum Gate<Fr: BinaryField> {
     // x_inv * x = flag
     // x * flag = X
     // out = x_inv * flag + x * (1 - flag)
-    InvSelectorGadget([Variable; 4]),
+    InvSelectorGate([Variable; 4]),
 
     // used for SubBytes : uses x from previous state (it's the first argument)
     // current state is (y1, y2, 23, out)
@@ -236,7 +267,7 @@ pub enum Gate<Fr: BinaryField> {
     // y1 = x^4, y2 = y1^4=x^16, y2 = y1^4 = x^64, 
     // subfield check: x = y2^4 = x^256
     // out = \sum c_i x^{2^i}, for i \in [0, 7]
-    SubBytesGagdet([Variable; 4]),
+    SubBytesGate([Variable; 4]),
 
     // MixClolumnGadget: arguments are [OUT, P0, P1, P2, P3]
     // as in composeGadget we assume that P3 was defined on previous row
@@ -244,9 +275,45 @@ pub enum Gate<Fr: BinaryField> {
     // [Q0, Q1, Q2, Q3]^(T)  = M * [P0, P1, P2, P3] ^ (T)
     // and composition: OUT = Q = Q0 * + Q1 * s + Q2 * s^2 + Q3 * s^3  
     // so it reduces to some linear combination of P0, P1, P2, P3
-    
+    MixColumnGate([Variable; 5]),
+
+    //simultaneous AddRoundKey + GenerateNextRoundLey
+    // let temp = SubBytes(Rot(Key))
+    // then with the state (P_old, P_new, K_old, K_new) (temp is a hidden variable)
+    // we have P_new = P_old + K_old
+    // K_new = K_old + temp and continet
+    RoundKeyAddUpdateGate([Variable; 5]),
+
+    // NB: there is a special optimization for the final round though!
+    // first combine then xor!
 
 
+    // this used for 128-bit field and Hirose Hash construction with 256bit output
+    // her state width is 8
+
+    // Compose Decompose for state of width 8
+    // P = [P0, P1, P2, P3]
+    ComposeWideGate([Variable; 5]),
+
+    // for width8 we may actually compose InvSelect and SubBytes in one row
+    // the state will be then (x, x_inv, flag, out, out^4, out^16, out^64, res_sum)
+    SubByteWideGate([Variable; 8]),
+
+    MixColumnsWideGate([Variable; 5]),
+
+    // for HiroseSheme the key is th same for both "subciphers"
+    // so we may simultaneouly multiple P and Q by the same round key and update the key
+    // the state is [P_old, Q_old, K_old, P_new, Q_new, temp, K_new]
+    RoundKeyAddUpdateWideGate([Variable; 7]),
+
+    // in the final key addition there is no need to update the key:
+    // we assume that all variables in the final key addition are composed back 10 128-bit width
+    // [L, R] is the "state" of Hirose constuction (Left and Right)
+    // the state is : [P, Q, L_old, R_old, K, L_new, R_new]
+    //the transition function are actually the following: 
+    // L_new = L_old + P + K
+    // R_new = R_old + Q + K
+    FinalHashUpdateWideGate([Variable; 7]),
 }
 
 // We also need inversion in Field which is implemented using  the following PAIR of MUL gates:
@@ -313,9 +380,79 @@ impl<Fr: BinaryField> Gate<Fr> {
             Gate::LongLinearCombinationGate(_, _) => GateType::LongLinearCombinationGate,
             Gate::SelectorGate(_) => GateType::SelectorGate,
             Gate::EqualityGate(_) => GateType::EqualityGate,
+
+            Gate::DecomposeGate(_) => GateType::DecomposeGate,
+            Gate::ComposeGate(_) => GateType::ComposeGate,
+            Gate::InvSelectorGate(_) => GateType::InvSelectorGate,
+            Gate::SubBytesGate(_) => GateType::SubBytesGate,
+            Gate::MixColumnGate(_) => GateType::MixColumnsWideGate,
+            Gate::RoundKeyAddUpdateGate(_) => GateType::RoundKeyAddUpdateGate,
+
+            Gate::ComposeWideGate(_) => GateType::ComposeWideGate,
+            Gate::SubByteWideGate(_) => GateType::SubByteWideGate,
+            Gate::MixColumnsWideGate(_) => GateType::MixColumnsWideGate,
+            Gate::RoundKeyAddUpdateWideGate(_) => GateType::RoundKeyAddUpdateWideGate,
+            Gate::FinalHashUpdateWideGate(_) => GateType::FinalHashUpdateWideGate,
+
             _ => unreachable!(),
         };
 
         res
     }
+
+    pub(crate) fn new_decompose_gate(P: Variable, P0: Variable, P1: Variable, P2: Variable, P3: Variable) -> Self {
+        Self::DecomposeGate([P, P0, P1, P2, P3])
+    }
+
+    pub(crate) fn new_compose_gate(P: Variable, P0: Variable, P1: Variable, P2: Variable, P3: Variable) -> Self {
+        Self::ComposeGate([P, P0, P1, P2, P3])
+    }
+
+    pub(crate) fn new_inv_select_gate(x: Variable, x_inv: Variable, flag: Variable, out: Variable) -> Self {
+        Self::InvSelectorGate([x, x_inv, flag, out])
+    }
+
+    pub(crate) fn new_sub_bytes_gate(x4: Variable, x16: Variable, x64: Variable, out: Variable) -> Self {
+        Self::SubBytesGate([x4, x16, x64, out])
+    }
+
+    pub(crate) fn new_mix_column_gate(OUT: Variable, P0: Variable, P1: Variable, P2: Variable, P3: Variable) -> Self {
+        Self::MixColumnGate([OUT, P0, P1, P2, P3])
+    }
+
+    pub(crate) fn new_add_undate_round_key_gate(
+        P_old: Variable, P_new: Variable, K_old: Variable, K_new: Variable, temp: Variable) -> Self {
+        
+        Self::RoundKeyAddUpdateGate([P_old, P_new, K_old, K_new, temp])
+    }
+
+
+    pub(crate) fn new_compose_wide_gate(P: Variable, P0: Variable, P1: Variable, P2: Variable, P3: Variable) -> Self {
+        Self::ComposeWideGate([P, P0, P1, P2, P3])
+    }
+
+    pub(crate) fn new_compose_wide_gate(P: Variable, P0: Variable, P1: Variable, P2: Variable, P3: Variable) -> Self {
+        Self::ComposeWideGate([P, P0, P1, P2, P3])
+    }
+    
+
+    // for width8 we may actually compose InvSelect and SubBytes in one row
+    // the state will be then (x, x_inv, flag, out, out^4, out^16, out^64, res_sum)
+    SubByteWideGate([Variable; 8]),
+
+    MixColumnsWideGate([Variable; 5]),
+
+    // for HiroseSheme the key is th same for both "subciphers"
+    // so we may simultaneouly multiple P and Q by the same round key and update the key
+    // the state is [P_old, Q_old, K_old, P_new, Q_new, temp, K_new]
+    RoundKeyAddUpdateWideGate([Variable; 7]),
+
+    // in the final key addition there is no need to update the key:
+    // we assume that all variables in the final key addition are composed back 10 128-bit width
+    // [L, R] is the "state" of Hirose constuction (Left and Right)
+    // the state is : [P, Q, L_old, R_old, K, L_new, R_new]
+    //the transition function are actually the following: 
+    // L_new = L_old + P + K
+    // R_new = R_old + Q + K
+    FinalHashUpdateWideGate([Variable; 7]),
 }
